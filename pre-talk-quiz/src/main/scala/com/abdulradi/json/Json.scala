9571f411e158034963d25edaae21ed3b2ff10e51
Mohammad Khaled Abou El Sherbini <mh-sherbini@hotmail.com>
2018-11-21 20:03:13 +0200

    Update Json.scala

--- pre-talk-quiz/src/main/scala/com/abdulradi/json/Json.scala
+++ pre-talk-quiz/src/main/scala/com/abdulradi/json/Json.scala
@@ -11,16 +11,23 @@ final case class JsonBoolean(value: Boolean) extends Json
 final case class JsonArray(values: Seq[Json]) extends Json
 final case class JsonObject(values: Seq[(String, Json)]) extends Json
 
+
+
 object JsonSerialisation {
   /**
     * Converts Json instance to it's string representation
     */
+  def main(args: Array[String]): Unit = {
+
+  }
+
   def serialise(json: Json): String = json match {
     case JsonNull => "null"
-    case JsonString(value) => ???
+    case JsonString(value) => "\"" +value+"\""
     case JsonNumber(value) => value.toString
-    case JsonBoolean(value) => ???
+    case JsonBoolean(value) => if (value == false) "false" else "true"
     case JsonArray(values) => values.map(v => serialise(v)).mkString("[", ",", "]")
-    case JsonObject(values) => ???
+    case JsonObject(values) => values.map(a=> "\"" +a._1+"\""+":"+ serialise(a._2)).mkString("{", ",", "}")
+    //why can't i use serialise with a._1
   }
 }
